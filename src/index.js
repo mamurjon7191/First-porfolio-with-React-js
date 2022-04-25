1bda6f02d752dc2a1eed465348ed76fd4b8a2e1e
Ma'murjon <mamurjon@Air-Mamurjon.mshome.net>
2022-04-25 09:58:09 +0500

    samandar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+// Har doim srcni ichida index.js yaratiladi
+
+import React from "react"; // reactni narsalarini olib keltiradi
+import ReactDOM from "react-dom"; // rendor qlish uchun kerak
+
+import App from "./components/App.js";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
